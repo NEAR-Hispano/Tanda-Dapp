badd3bc74d567ed00c2bf9800b7266ad61c8d122
JoyceLozanoOP <joycseleneanaid@gmail.com>
2021-08-26 00:17:16 -0500

    Se añade método getTanda por nombreTanda

--- tanda-dapp/contract/assembly/index.ts
+++ tanda-dapp/contract/assembly/index.ts
@@ -1,13 +1,13 @@
-import { Context, logging, storage, PersistentVector } from 'near-sdk-as'
-import { Tanda, Periodo } from "./model";
+import { logging } from 'near-sdk-as'
+import { tandas, Tanda} from "../models/tanda";
 
-const account_id = "aklassen.testnet";
-
-let tandas = new PersistentVector<Tanda>("t");
+const account_id = "";
 
 export function setTanda(nombreTanda: string, integrantes: u64, monto: u64): void{
 
   let tanda = new Tanda(nombreTanda, integrantes, monto);
+
+  //tanda.periodo = Periodo;
 
   logging.log(
     'Creando tanda"' 
@@ -15,9 +15,11 @@ export function setTanda(nombreTanda: string, integrantes: u64, monto: u64): voi
       + '" en la cuenta "' 
       + account_id
       + '" con "'
-      + integrantes.toString()
+      + "8"
       + '" integrantes, por "'
-      + monto.toString()
+      + "3.0"
+      + '" cada "'
+      + '"'
   )
   tandas.push(tanda);
 }
@@ -30,4 +32,15 @@ export function getTandas(): Array<Tanda>{
     result[i] = tandas[i + startIndex];
   }
   return result;
+}
+
+export function getTanda(nombreTanda: string): Tanda | null {
+  let numTandas = min(10, tandas.length);
+  const startIndex = tandas.length - numTandas;
+  for (let i = 0; i < numTandas; i++) {
+    if (tandas[i + startIndex].nombre == nombreTanda) {
+      return tandas[i + startIndex];
+    }
+  }
+  return null;
 }
