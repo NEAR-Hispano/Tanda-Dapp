22e64ce175cc7a75b329e53d14eca1fe1e1e2754
Ayrton Klassen <ayrton.klassen@gmail.com>
2021-08-31 00:35:48 -0500

    Cambios a Tanda

--- tanda-dapp/contract/assembly/index.ts
+++ tanda-dapp/contract/assembly/index.ts
@@ -1,11 +1,12 @@
 import { logging } from 'near-sdk-as'
-import { tandas, keys, Tanda} from "../models/tanda";
+import { tandas, keys, Tanda, periodos} from "../models/tanda";
+//import { Date } from "as-date";
 
 const account_id = "";
 
-export function setTanda(nombreTanda: string, integrantes: u64, monto: u64): void{
+export function setTanda(nombreTanda: string, integrantes: u64, monto: u64, periodo: i32): void{
 
-  let tanda = new Tanda(nombreTanda, integrantes, monto);
+  let tanda = new Tanda(nombreTanda, integrantes, monto, periodo);
 
   //tanda.periodo = Periodo;
 
@@ -15,10 +16,11 @@ export function setTanda(nombreTanda: string, integrantes: u64, monto: u64): voi
       + '" en la cuenta "' 
       + account_id
       + '" con "'
-      + "8"
+      + integrantes.toString()
       + '" integrantes, por "'
-      + "3.0"
-      + '" cada "'
+      + monto.toString()
+      + '" con un periodo "'
+      + periodos.get(periodo)
       + '"'
   )
   tandas.set(tanda.id, tanda);
@@ -37,4 +39,29 @@ export function getTandas(): Array<Tanda | null>{
 
 export function getTanda(key: string): Tanda | null {
   return tandas.get(key);
+}
+
+export function cambiarEstadoTanda(key: string): Tanda | null {
+
+  let tanda = tandas.get(key);
+  tandas.delete(key);
+
+  
+  if(tanda != null){
+    if(tanda.activa){
+      tanda.activa = false;
+      tanda.fecha_final = new Date(1630387618);
+    }
+    else{
+      let dias_a_sumar = tanda.num_integrantes * tanda.periodo;
+      tanda.activa = true;
+      //tanda.fecha_inicio = new Date(Date.now());
+      //tanda.fecha_final = new Date(Date.now() + dias_a_sumar);
+    }
+
+    tandas.set(tanda.id, tanda);
+    return tanda;
+  }
+
+  return null;
 }
--- tanda-dapp/contract/models/tanda.ts
+++ tanda-dapp/contract/models/tanda.ts
@@ -1,4 +1,5 @@
 import { context, u128, PersistentMap, PersistentVector, math } from "near-sdk-as";
+//import { Date } from "as-date";
 
 /**
  * Exportando la clase Tanda para poder utilizarla desde otros archivos
@@ -9,14 +10,24 @@ export class Tanda {
     nombre: string;
     num_integrantes: u64;
     monto: u64;
+
+    fecha_inicio: Date;
+    fecha_final: Date;
+    activa: bool;
+
+    periodo: u64;
     
-    constructor(nombre: string, num_integrantes: u64, monto: u64){
+    constructor(nombre: string, num_integrantes: u64, monto: u64, periodo: u64){
         this.id = math.randomBuffer(14).toString();
         this.nombre = nombre;
         this.num_integrantes = num_integrantes;
         this.monto = monto;
+        this.activa = false;
+        this.periodo = periodo;
+
+        //this.fecha_final = new Date(0);
+        //this.fecha_inicio = new Date(0);
     }
-    
 }
 
 /**
@@ -27,3 +38,10 @@ export class Tanda {
  */
  export const tandas = new PersistentMap<string, Tanda>("m");
  export const keys = new PersistentVector<string>("k");
+
+ //Creando tipo Periodo
+ export const periodos = new Map<i32, string>();
+
+ periodos.set(7, "Semanal");
+ periodos.set(15, "Quincenal");
+ periodos.set(30, "Mensual");
