c447d12074b5cd7e7747f31862c8537e058e3d64
JoyceLozanoOP <joycseleneanaid@gmail.com>
2021-10-26 00:41:17 -0500

    Se añade logo y favicon del logo de tandem

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { logout } from './../utils';
-import { Layout, Button, Avatar, Typography } from 'antd';
+import { Layout, Button, Avatar, Typography, Image } from 'antd';
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import ImageLogo from '../assets/tandem.png';
 
 import 'antd/dist/antd.css';
 
@@ -12,11 +13,14 @@ const { Header } = Layout;
 export const Encabezado = () => {
     return (
         <Header>
-            <p style={{ textAlign: 'right', marginTop: '2.5em',  marginRight: '1em'}}>
+            <p style={{ textAlign: 'right', marginTop: '2.5em',  marginRight: '1em'}}>               
                 <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} /> 
                 <Text style={{color:'white', marginLeft:'0.5em', marginRight:'1em'}}>{window.accountId}!</Text>
                 <Button  type="primary"  shape="round" ghost icon={<LogoutOutlined />} onClick={logout}>Cerrar sesión</Button>
             </p>
+            <div>
+                <Image width={100} src={ImageLogo} preview={false} style={{margin:'1.5em'}}/>
+           </div>
         </Header>
     )
 }
