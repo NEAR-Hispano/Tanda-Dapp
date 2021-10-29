4ae913ff67f6af1ea772f6736aae92d5af15806e
JoyceLozanoOP <joycseleneanaid@gmail.com>
2021-10-28 22:22:49 -0500

    Se elimina backend del contrato

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { initContract } from './utils'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { initContract } from './utils';
 import 'tachyons'; 
 
 window.nearInitPromise = initContract()
